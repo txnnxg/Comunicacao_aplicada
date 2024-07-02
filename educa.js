49919c787838634d204b04fdc09554b29a5e156e
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-07-02 00:59:30 -0300

    90% do site pronto

--- educa.js
+++ educa.js
@@ -3,3 +3,11 @@ function carregar() {
     var nome = window.document.getElementById('nome')
     nome.innerHTML = `Seja bem vindo ${res}`
 }
+var imagem = document.getElementById("imagem")
+ 
+function bigImg(x) {
+  return imagem.innerHTML = "<img src='imagens/mapamental2.png' />"
+}
+function normalImg(x) {
+    return imagem.innerHTML = "Infográfico"
+}
--- reveal.js
+++ reveal.js
@@ -4,6 +4,13 @@ window.revelar = ScrollReveal({reset:true})
 revelar.reveal('.efeito-txt-topo',
 {
     duration: 2000,
-    distance: '90px',
-    origin: 'right'
+    distance: '500px',
+    origin: 'right',
+    delay: '100'
 })
+revelar.reveal('.p3',
+    {
+        duration: 2500,
+        distance: '90px',
+        origin: 'bottom'
+    })
