a8eb2d8994016e2108ece5a49a356861867dbcbf
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-07-02 10:25:18 -0300

    infográfico adicionado responsivamente

--- educa.js
+++ educa.js
@@ -3,11 +3,30 @@ function carregar() {
     var nome = window.document.getElementById('nome')
     nome.innerHTML = `Seja bem vindo ${res}`
 }
-var imagem = document.getElementById("imagem")
+    var imagem = document.getElementById("imagem")
  
-function bigImg(x) {
-  return imagem.innerHTML = "<img src='imagens/mapamental2.png' />"
+    function bigImg(x) {
+      return imagem.innerHTML = "<img src='imagens/mapamental2.png' />"
+    }
+    function normalImg(x) {
+        return imagem.innerHTML = "Infográfico"
+    }
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    pic = document.getElementById("imagem")
+    pic.style.visibility = "hidden"
+    fot = document.getElementById("fotosozinha")
+    fot.innerHTML = "<img src='imagens/mapamental2.png' />"
+  } else{
+    pic.style.visibility = "visible"
+  }
 }
-function normalImg(x) {
-    return imagem.innerHTML = "Infográfico"
-}
+
+var x = window.matchMedia("(max-width: 849px)")
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+  myFunction(x);
+});
